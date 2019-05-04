89b0ba00b394197a0630291280ffccf59237aa5e
olaven <olavsundfoer@gmail.com>
2019-05-04 17:33:26 +0200

    expanded user-data

--- backend/src/main/resources/db/migration/V1.0__createSql.sql
+++ backend/src/main/resources/db/migration/V1.0__createSql.sql
@@ -1,4 +1,5 @@
-create sequence hibernate_sequence start with 1 increment by 1;
-create table user_entity (username varchar(255) not null, enabled boolean not null, password varchar(255), primary key (username));
-create table user_entity_roles (user_entity_username varchar(255) not null, roles varchar(255));
-alter table user_entity_roles add constraint FKnkblne5vj59d4pbt8r3n8tifd foreign key (user_entity_username) references user_entity;
+drop table user_entity if exists;
+drop table user_entity_roles if exists;
+create table user_entity (email varchar(255) not null, enabled boolean not null, family_name varchar(55), given_name varchar(55), password varchar(340), primary key (email));
+create table user_entity_roles (user_entity_email varchar(255) not null, roles varchar(255));
+alter table user_entity_roles add constraint FKn6hs2xulrxb06oc101wxtihwv foreign key (user_entity_email) references user_entity;
